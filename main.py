eda89f25fa17e7e8d31dbd861b72ed05d5134977
Thomasvgx <vigroux.thomas82@gmail.com>
2018-07-11 18:21:41 +0200

    Ajout menu sélection

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import Tkinter
+from Tkinter import *
 from tkMessageBox import *
 
-class simpleapp_tk(Tkinter.Tk):
+
+class menu_depart(Tk):
+
+	def __init__(self):
+		Tk.__init__(self)
+		self.grid()
+		self.title("Menu Marqueur")
+		# logo = PhotoImage(file = "KBM.gif")
+		# CanvasLogo = Canvas(self, width = logo.width(), height = logo.height())
+		# CanvasLogo.create_image(0,0, anchor = NW, image = logo)
+		#CanvasLogo.grid(row = 0, column = 0)
+
+		Bouton_simple = Button(self, text="Mode match simple",command=self.run_simple)
+		Bouton_simple_o = Button(self, text="Mode match simple officiel", command=self.run_simple_off)
+		Bouton_quitter = Button(self, text="Quitter", command=quit)
+
+		Bouton_simple.grid(row = 0, column = 0)
+		Bouton_simple_o.grid(row = 0, column = 1)
+		Bouton_quitter.grid(row=0, column = 2)
+
+	def run_simple(self):
+		global choix
+		choix = 1
+		self.destroy()
+	def run_simple_off(self):
+		global choix
+		choix = 2
+		self.destroy()
+
+class marqueur(Tk):
 
 	G = 0
 	N = 0
@@ -13,66 +42,64 @@ class simpleapp_tk(Tkinter.Tk):
 	taille = 220
 	chainetaille = "-size " + str(taille)
 
-	def __init__(self,parent):
-		Tkinter.Tk.__init__(self,parent)
-		self.parent = parent
-		self.initialize()
 
-	def initialize(self):
+	def __init__(self, nom):
+		Tk.__init__(self)
+		self.title(nom)
 		self.grid()
 
-		self.Gn = Tkinter.IntVar()
-		self.Nn = Tkinter.IntVar()
-		self.Bn = Tkinter.IntVar()
+		self.Gn = IntVar()
+		self.Nn = IntVar()
+		self.Bn = IntVar()
 
 		self.rowconfigure(0, weight=1)
 
-		CanvasBleu = Tkinter.Canvas(self, width = 300, height = 600, background = 'blue')
+		CanvasBleu = Canvas(self, width = 300, height = 600, background = 'blue')
 		CanvasBleu.grid(row = 0, column = 1, sticky="nsew") 
 		self.columnconfigure(2, weight=1)
-		CanvasGris = Tkinter.Canvas(self, width = 300, height = 600, background = 'grey')
+		CanvasGris = Canvas(self, width = 300, height = 600, background = 'grey')
 		CanvasGris.grid(row = 0, column = 2, sticky="nsew")
 		self.columnconfigure(1, weight=1)
-		CanvasNoir = Tkinter.Canvas(self, width = 300, height = 600, background = 'black')
+		CanvasNoir = Canvas(self, width = 300, height = 600, background = 'black')
 		CanvasNoir.grid(row = 0, column = 3, sticky="nsew") 
 		self.columnconfigure(3, weight=1)
 
-		nombleu = Tkinter.Label(self, text="Kin-Ball Montalbanais", bg = 'blue')
-		nombleu.configure(font="-size 20")
-		nombleu.grid(column = 1, row = 0, sticky="n")
+		# nombleu = Label(self, text="Kin-Ball Montalbanais", bg = 'blue')
+		# nombleu.configure(font="-size 20")
+		# nombleu.grid(column = 1, row = 0, sticky="n")
 
-		labelbleu = Tkinter.Label(self, textvariable=self.Bn, bg = 'blue')
+		labelbleu = Label(self, textvariable=self.Bn, bg = 'blue')
 		labelbleu.configure(font=self.chainetaille)
 		labelbleu.grid(column = 1, row = 0)
 
-		labelgris = Tkinter.Label(self, textvariable=self.Gn, bg = 'grey')
+		labelgris = Label(self, textvariable=self.Gn, bg = 'grey')
 		labelgris.configure(font=self.chainetaille)
 		labelgris.grid(column = 2, row = 0)
 
 
-		labelnoir = Tkinter.Label(self, textvariable=self.Nn, bg = 'black', fg = 'white')
+		labelnoir = Label(self, textvariable=self.Nn, bg = 'black', fg = 'white')
 		labelnoir.configure(font=self.chainetaille)
 		labelnoir.grid(column = 3, row = 0)
 
-		BoutonBleu = Tkinter.Button(self, text="Faute aux Bleus", command = self.fauteB).grid(row=2,column=1)
-		BoutonGris = Tkinter.Button(self, text="Faute aux Gris", command = self.fauteG).grid(row=2,column=2)
-		BoutonNoir = Tkinter.Button(self, text="Faute aux Noirs", command = self.fauteN).grid(row=2,column=3)
+		BoutonBleu = Button(self, text="Faute aux Bleus", command = self.fauteB).grid(row=2,column=1)
+		BoutonGris = Button(self, text="Faute aux Gris", command = self.fauteG).grid(row=2,column=2)
+		BoutonNoir = Button(self, text="Faute aux Noirs", command = self.fauteN).grid(row=2,column=3)
 
 		## Début Barre de menu
 
-		BarreMenu = Tkinter.Menu(self)
+		BarreMenu = Menu(self)
 
-		fichier = Tkinter.Menu(BarreMenu, tearoff=0)
+		fichier = Menu(BarreMenu, tearoff=0)
 		fichier.add_command(label="Nouveau ..", command = quit)
 		fichier.add_separator()
 		fichier.add_command(label="Enregister ..", command = quit)
 		fichier.add_command(label="Enregistrer sous ..", command = quit)
 
-		affichage = Tkinter.Menu(BarreMenu, tearoff=0)
+		affichage = Menu(BarreMenu, tearoff=0)
 		affichage.add_command(label="Plein écran ..", command = self.pleinecran)
 		affichage.add_command(label="Quitter plein écran ..", command = self.quitpleinecran)
 
-		aide = Tkinter.Menu(BarreMenu, tearoff = 0)
+		aide = Menu(BarreMenu, tearoff = 0)
 		aide.add_command(label="Aide en ligne")
 		aide.add_separator()
 		aide.add_command(label="Crédits", command = self.credit)
@@ -132,6 +159,7 @@ class simpleapp_tk(Tkinter.Tk):
 		else: self.equipedehors = None
 
 	def fauteN(self):
+		#self.clignotement("Noir")
 		self.G = self.G + 1
 		self.B = self.B + 1
 		self.Gn.set(self.G)
@@ -153,6 +181,16 @@ class simpleapp_tk(Tkinter.Tk):
 		self.Bn.set(self.B)
 		self.comparaison()
 
+	# def clignotement(self, couleur):
+	# 	global label_status
+	# 	if (label_status == 'place'):
+	# 		self.labelnoir.grid_forget()
+	# 		label_status = 'not_place'
+	# 	elif (label_status == 'not_place'):
+	# 		self.labelnoir.grid(column = 3, row = 0)
+	# 		label_status = 'place'
+	# 	window.after(100, clignotement)
+		
 	def pleinecran(self):
 		self.attributes('-fullscreen', 1)
 
@@ -175,8 +213,13 @@ class simpleapp_tk(Tkinter.Tk):
 
 		
 if __name__ == '__main__':
+	menu = menu_depart()
+	menu.mainloop()
+	if choix == 1:
+		marqueur = marqueur("Match simple")
+		marqueur.mainloop()
+	elif choix == 2:
+		marqueur = marqueur("Match simple officiel")
+		marqueur.mainloop()
 
-		app = simpleapp_tk(None)
-		app.title('Marqueur Kin-Ball')
-		app.mainloop()
 		
