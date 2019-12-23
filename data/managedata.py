e90bdae28c22aab3e0fbfa27ff2ae02677a99f29
Thomas <vigroux.thomas82@gmail.com>
2019-12-23 14:04:23 +0100

    typo fix

--- data/managedata.py
+++ data/managedata.py
@@ -2,7 +2,7 @@ class dataManager():
     """ This class manage all data about scores and teams """
     def __init__(self):
         """ Constructor """
-        choix = 0
-        nameBlueTeam = ""
-        nameGreyTeam = ""
-        nameBlackTeam = ""
+        self.choix = 0
+        self.nameBlueTeam = ""
+        self.nameGreyTeam = ""
+        self.nameBlackTeam = ""
--- main.py
+++ main.py
@@ -9,20 +9,12 @@ from display.app     import marqueur
 from data.managedata import dataManager
 
 if __name__ == '__main__':
-	data = dataManager()
-	menu = menu_depart(data)
-	menu.mainloop()
-	
-	if data.choix == 1:
-		selectTeam = menu_selection_equipe(data)
-		selectTeam.mainloop()
-		marqueur = marqueur("Match simple", data)
-		marqueur.mainloop()
-	
-	# elif choix == 2:
-	# 	selection = menu_selection_equipe()
-	# 	selection.mainloop()
-	# 	marqueur = marqueur("Match simple officiel")
-	# 	marqueur.mainloop()
+    data = dataManager()
+    menu = menu_depart(data)
+    menu.mainloop()
 
-		
+    if data.choix == 1:
+        selectTeam = menu_selection_equipe(data)
+        selectTeam.mainloop()
+        marqueur = marqueur("Match simple", data)
+        marqueur.mainloop()
